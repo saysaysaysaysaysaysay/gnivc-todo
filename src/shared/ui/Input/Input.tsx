352d65c2cd06aad4808549ea3bf4d468782c6295
UselessMiva <ivnickaba@gmail.com>
2026-07-16 10:57:00 +0400

    refactor(shared/ui): edit Input props and styles

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,54 +1,34 @@
-import { memo, type ChangeEvent, useCallback } from 'react';
+import { memo, useId} from 'react';
 import type { IInputProps } from './types';
 import styles from './Input.module.scss';
 
 function InputImpl({
-  value,
-  onChange,
-  onBlur,
-  placeholder,
   label,
-  error,
-  required = false,
-  className = '',
-  type = 'text',
-  name,
+  error, 
   id,
-  maxLength,
-  autoFocus = false,
+  className = '',
+  ...props
 }: IInputProps) {
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(e.target.value);
-  }, [onChange]);
-
-  const handleBlur = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    onBlur?.(e.target.value);
-  }, [onBlur]);
-
+  const generatedId = useId()
+  const inputId = id ?? generatedId
   return (
     <div className={`${styles.inputWrapper} ${className}`}>
+
       {label && (
-        <label htmlFor={id} className={styles.label}>
+        <label htmlFor={inputId} className={styles.label}>
           {label}
-          {required && <span className={styles.required}>*</span>}
         </label>
       )}
       
       <input
-        id={id}
-        name={name}
-        type={type}
-        value={value}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        placeholder={placeholder}
-        required={required}
-        maxLength={maxLength}
-        autoFocus={autoFocus}
-        className={`${styles.input} ${error ? styles.error : ''}`}
+        id={inputId}
+        className={`${styles.input}`}
+        aria-invalid={Boolean(error)}
+        {...props}
       />
       
       {error && <span className={styles.errorMessage}>{error}</span>}
+
     </div>
   );
 }
--- src/shared/ui/Input/types.ts
+++ src/shared/ui/Input/types.ts
@@ -1,15 +1,6 @@
-export interface IInputProps {
-  value?: string;
-  onChange?: (value: string) => void;
-  onBlur?: (value: string) => void;
-  placeholder?: string;
+import type { InputHTMLAttributes } from "react";
+
+export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   error?: string;
-  required?: boolean;
-  className?: string;
-  type?: "text" | "email" | "password" | "number" | "tel" | "url";
-  name?: string;
-  id?: string;
-  maxLength?: number;
-  autoFocus?: boolean;
 }
