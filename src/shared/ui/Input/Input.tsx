cc2a156ab8f2b0095d6e036ae3b2b0c34d346afe
UselessMiva <ivnickaba@gmail.com>
2026-07-22 11:59:20 +0400

    feat(shared/input): add clear button for input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,36 +1,54 @@
-import { useId} from 'react';
+import { useId, useRef } from 'react';
 import clsx from 'clsx';
 import type { IInputProps } from './types';
 import styles from './Input.module.scss';
+import { Button } from '@/shared/ui';
+import { FaX } from 'react-icons/fa6';
 
 export function Input({
   label,
-  error, 
+  error,
   id,
   className = '',
   ...props
 }: IInputProps) {
-  const generatedId = useId()
-  const inputId = id ?? generatedId
-  
+  const generatedId = useId();
+  const inputId = id ?? generatedId;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleReset() {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+    inputRef.current?.focus();
+  }
+
   return (
     <div className={clsx(styles['input-wrapper'], className)}>
-
       {label && (
         <label htmlFor={inputId} className={styles.label}>
           {label}
         </label>
       )}
-      
-      <input
-        id={inputId}
-        className={styles.input}
+      <div
+        className={styles['input-container']}
         aria-invalid={Boolean(error)}
-        {...props}
-      />
-      
+      >
+        <input
+          id={inputId}
+          className={styles.input}
+          ref={inputRef}
+          {...props}
+        />
+          <Button
+            aria-label="Очистить поле"
+            className={styles['input-reset']}
+            onClick={handleReset}
+          >
+            <FaX />
+          </Button>
+      </div>
       {error && <span className={styles['error-message']}>{error}</span>}
-
     </div>
   );
 }
